1a0bf111f09b22fb91a633f66820b5053aae888f
adamisrusty <adamgutierreztx@gmail.com>
2020-09-10 17:22:24 -0500

    Initial commit, adding file reading and json serialization

---
+++ dotTrackerCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotTrackerCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
